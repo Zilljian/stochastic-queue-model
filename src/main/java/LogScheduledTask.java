e9bdde0359e5950a29ce35b15f74013ab4b65843
Ilya Balakin <victorieuse48@gmaili.com>
2020-03-13 01:40:42 +0300

    Add execution counter.

--- src/main/java/LogScheduledTask.java
+++ src/main/java/LogScheduledTask.java
@@ -11,11 +11,16 @@ public class LogScheduledTask extends Thread {
     private final Logic logic;
     private FileWriter out;
     private long second = 0;
+    private static int executionCounter = 0;
+    private final static int MAX = 200;
     private final String[] HEADERS = {"second", "success", "rejected", "current_size"};
 
     @Override
     @SneakyThrows
     public void run() {
+        if (executionCounter++ == MAX) {
+            System.exit(0);
+        }
         out = new FileWriter("queue_metrics.csv", true);
         try (CSVPrinter printer = second == 0 ?
                 new CSVPrinter(out, CSVFormat.DEFAULT.withHeader(HEADERS)) :
